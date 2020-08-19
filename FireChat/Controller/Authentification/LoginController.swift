743bd57b38b9c9cbd31ddf20c4f5e45f33ef8977
Merouane Bellaha <m.bellaha@gmail.com>
2020-08-19 10:49:41 +0200

    UI setup in progress

---
+++ FireChat/Controller/Authentification/LoginController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginController.swift
+//  FireChat
+//
+//  Created by Merouane Bellaha on 19/08/2020.
+//  Copyright © 2020 Merouane Bellaha. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+
+    private let iconImage: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "bubble.right")
+        imageView.tintColor = .white
+        return imageView
+    }()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    // MARK: - Helpers
+
+    func configureUI() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+
+        configureGradientLayer()
+
+        view.addSubview(iconImage)
+        iconImage.translatesAutoresizingMaskIntoConstraints = false
+        iconImage.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        iconImage.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        iconImage.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        iconImage.widthAnchor.constraint(equalToConstant: 120).isActive = true
+
+    }
+
+    func configureGradientLayer() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+}
